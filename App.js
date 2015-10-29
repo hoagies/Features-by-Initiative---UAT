ff82a7df0dc0e07f4448c2604e061ad401813924
hoagies <sphogan85@gmail.com>
2015-10-29 17:41:28 +0000

    updating

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 	launch: function() {
-		
-		// Radian
-		// var project_oid = '/project/37192747640';
 
 		this.add({
 			xtype: 'rallycombobox',
@@ -29,15 +26,9 @@ Ext.define('CustomApp', {
 				remoteGroup: false,
 				remoteSort: false,
 				remoteFilter: false,
-				limit: Infinity,
-				// context: {
-					// project: project_oid,
-					// projectScopeDown: true,
-					// projectScopeUp: false
-				// }
+				limit: Infinity
 			},
 			listeners: {
-				// select: this._onSelect,
 				select: this._onLoad,
 				ready: this._onLoad,
 				scope: this
@@ -46,140 +37,69 @@ Ext.define('CustomApp', {
 	},
 		
 	_onLoad: function() {
-		var project_oid = '/project/37192747640';
+		console.log('changing');
 		
-		if (this.down('#features')) {
-			this.down('#features').destroy();
+		if (this.down('#features_uat')) {
+			this.down('#features_uat').destroy();
 		}
 		
-		var store = "";
-		
 		this.add({
-			id: 'features',
-			xtype: 'rallygridboard',
-			modelNames: ['PortfolioItem/Feature'],
+			xtype: 'rallycardboard',
+			id: 'features_uat',
+			// types: ['User Story'],
+			types: ['PortfolioItem/Feature'],
+			attribute: 'c_UATState',
 			context: this.getContext(),
 			storeConfig: {
-				context: {
-					project: project_oid,
-					projectScopeDown: true,
-					projectScopeUp: false
-				},
-				filters: [this._getFilter()]
+				filters: [
+					this._getFilter()
+				]
 			},
 			columnConfig: {
 				plugins: [
 					{ptype: 'rallycolumncardcounter'}
 				]
 			},
-            // cardBoardConfig: {
-                // attribute: 'State',
-				// fetch: ['Project', 'PercentDoneByStoryCount'],
-				// showIconsAndHighlightBorder: false,
-				// editable: false,
-				// showAge: true
-            // },
-			cardBoardConfig: this._getBoardConfig(),
-			gridConfig: {
-				store: store,
-				columnCfgs: [
-					'Name',
-					'ScheduleState',
-					'Owner',
-					'PlanEstimate'
-				]
+			cardConfig: {
+				// fields: ['Feature','Iteration','Project'],
+				showIconsAndHighlightBorder: false,
+				editable: false,
+				showAge: true
 			},
-			// cardConfig: {
-				// xtype: 'rallycard',
-				// fields: ['State','Project']
-            // },
-			plugins: [
-				{
-					ptype: 'rallygridboardfieldpicker',
-					modelNames: ['PortfolioItem/Feature'],
-					headerPosition: 'left',
-					boardFieldDefaults: ['Project','PercentDoneByStoryCount']
-					// boardAlwaysSelectedValues: ['Project','PercentDoneByStoryCount']
-					// stateful: true,
-					// stateId: context.getScopedStateId('columns-example')
-				// }, {
-                    // ptype: 'rallygridboardcustomfiltercontrol',
-					// headerPosition: 'left',
-                    // filterControlConfig: {
-						// modelNames: ['PortfolioItem/Feature'],
-						// stateful: true,
-						// stateId: context.getScopedStateId('custom-filter-example')
-					// },
-                    // },
-					// showOwnerFilter: false
-					// ownerFilterControlConfig: {
-						// stateful: true,
-						// stateId: context.getScopedStateId('owner-filter-example')
-					// }
-                }
-			],
+			rowConfig: {
+				field: 'c_UATTeamMember'
+			},
 			listeners: {
-				load: this._onLoadBoard,
-				scope: this
+				load: function(board) {
+					var rows = board.getRows();
+					_.each(rows, function(row) {
+						var cardsInRow = board.getCardsInRow(row);
+						var cardCount = _.flatten(_.values(cardsInRow)).length;
+						if(cardCount === 0) {
+							row.collapse();
+						}else{
+							row.expand();
+						}
+					});
+				}
 			}
 		});
 	},
-	
-	// _onSelect: function() {
-
-	// },
 		
 	_getFilter: function() {
+		
 		var combo = this.down('rallycombobox');
-		return {
-			property: 'Parent.Parent',
-			operator: '=',
-			value: combo.getValue()
-		},
-		{
+		var filters = Ext.create('Rally.data.QueryFilter', {
 			property: 'c_UATTeamMember',
 			operator: '!=',
 			value: null
-		};
-	},
-
-	_getBoardConfig: function () {
-		return {
-			xtype: 'rallycardboard',
-			attribute: 'c_UATState',
-			rowConfig: {
-				field: 'c_UATTeamMember',
-				// sorters: [
-					// {
-						// property: this._getRankField(),
-						// direction: 'ASC'
-					// },
-					// {
-						// property: 'TaskIndex',
-						// direction: 'ASC'
-					// }
-				// ],
-				// headerConfig: {
-					// xtype: 'rallytaskboardrowheader'
-				// },
-				// sortField: this._getRankField(),
-				// enableCrossRowDragging: false
-			},
-			// margin: '10px 0 0 0',
-			// plugins: [{ptype:'rallyfixedheadercardboard'}]
-		};
-	},
-	
-	_onLoadBoard: function() {
-		var board = this.down('rallycardboard');
-		rows = board.getRows();
-		var rowsfilled = rows[0].columns[0]._cardsByRow;
-		console.log(rowsfilled);
-		// console.log(rows);
-		Ext.Array.each(rows, function(row) {
-			// row.collapse();
-			console.log(row.value);
 		});
-	},
+		filters = filters.and({
+			property: 'Parent.Parent',
+			operator: '=',
+			value: combo.getValue()
+		});
+		return filters;
+	}
 
 });
